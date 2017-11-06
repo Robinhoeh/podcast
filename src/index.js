8b038288bae76a970668fe9817392cd288bafeda
Robin Watson <drumsrobin@gmail.com>
2017-11-06 11:25:15 -0500

    nav almost working

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import $ from 'jquery';
 $(document).ready( () => {
   $('.hamburger').on('click', () => {
   	$('.menu').toggleClass('dropDown');
-  	$('.menu').hide('mobileMenu');
+  	$('.menu').toggleClass('show');
   });
 });
