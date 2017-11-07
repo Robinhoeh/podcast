572f59996c6addb7a1888894a8d95e8e1e5e1b09
Robin Watson <drumsrobin@gmail.com>
2017-11-07 11:45:28 -0500

    flex to dropdown

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import $ from 'jquery';
 $(document).ready( () => {
   $('.hamburger').on('click', () => {
   	$('.menu').toggleClass('dropDown');
-  	$('.menu ul').toggleClass('show').on('click', () => {
-  	});
+  	$('.menu ul').toggleClass('show');
   });
 });
 
